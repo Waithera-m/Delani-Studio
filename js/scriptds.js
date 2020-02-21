49af7b8b3afdaae2a7e1ad2caaba593e4497fcc8
Mary Waithera <njihiamary11@gmail.com>
2020-02-21 14:47:03 +0300

    Add and center About Us section using jQuery

---
+++ js/scriptds.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+  $("#header").css({
+    "text-align": "center",
+    "font-style": "bold",
+    "color": "white"
+  })
+  $("#header2").css({
+    "text-align": "center",
+    "color": "white"
+  })
+  $("#about").css({
+    "text-align":"center"
+  }
+  )
+}
+)
