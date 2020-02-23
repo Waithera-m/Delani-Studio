508c268c53b2d50bb17a79358926f8f377175ca8
Mary Waithera <njihiamary11@gmail.com>
2020-02-23 15:29:31 +0300

    Add overlay effect to portfolio icons using mouseenter, mouseleave, hide, and show

--- js/scriptds.js
+++ js/scriptds.js
@@ -10,32 +10,70 @@ $(document).ready(function(){
   });
   $("#identity").css({
     "text-align":"center"
-  }
-  );
+  });
   $("#services").css({
     "text-align":"center",
     "color":"white",
     "padding":"60px"
-  }
-  );
+  });
   $("#intouch").css({
     "color":"white",
-  }
-  );
+  });
+}
+);
+
+$(document).ready(function(){
   $(".clickable").click(function(){
     $("#arrow-hidden").slideToggle();
     $("#arrow-showing").slideToggle();
-  }
-  );
+  });
   $(".clickableOne").click(function(){
     $("#folder-hidden").slideToggle();
     $("#folder-showing").slideToggle();
-  }
-  );
+  });
   $(".clickableTwo").click(function(){
     $("#clipboard-hidden").slideToggle();
     $("#clipboard-showing").slideToggle();
-  }
-  )
-}
-)
+  });
+
+  $("#work4").mouseenter(function(){
+    $("#overlay").show();
+  }).mouseleave(function(){
+    $("#overlay").hide();
+  });
+  $("#work3").mouseenter(function(){
+    $("#overlayUn").show();
+  }).mouseleave(function(){
+    $("#overlayUn").hide();
+  });
+  $("#work2").mouseenter(function(){
+    $("#overlayDeux").show();
+  }).mouseleave(function(){
+    $("#overlayDeux").hide();
+  });
+  $("#work1").mouseenter(function(){
+    $("#overlayTrois").show();
+  }).mouseleave(function(){
+    $("#overlayTrois").hide();
+  });
+  $("#work5").mouseenter(function(){
+    $("#overlayQuatre").show();
+  }).mouseleave(function(){
+    $("#overlayQuatre").hide();
+  });
+  $("#work6").mouseenter(function(){
+    $("#overlayCinq").show();
+  }).mouseleave(function(){
+    $("#overlayCinq").hide();
+  });
+  $("#work7").mouseenter(function(){
+    $("#overlaySix").show();
+  }).mouseleave(function(){
+    $("#overlaySix").hide();
+  });
+  $("#work8").mouseenter(function(){
+    $("#overlaySept").show();
+  }).mouseleave(function(){
+    $("#overlaySept").hide();
+  });
+});
